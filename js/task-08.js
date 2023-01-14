b9af1b73c2557d3e84ab4fb28411784497d07415
MaksymKonin <skam707@gmail.com>
2023-01-15 01:37:57 +0200

    8-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (mail === "" || password === "") {
+    alert(`Поле Email або Password пусті. Заповніть всі поля!`);
+  } else {
+    const formData = {
+      mail,
+      password,
+    };
+    console.log(formData);
+    event.currentTarget.reset();
+  }
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const bodyEl = document.body;
+const buttonEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+buttonEl.addEventListener("click", onTargetButtonClick);
+
+function onTargetButtonClick(event) {
+  let color = getRandomHexColor();
+  bodyEl.style.background = `${color}`;
+  spanEl.textContent = `${color}`;
+  console.log(color);
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
